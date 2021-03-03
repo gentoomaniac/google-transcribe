ceb493acd9129ee2a5970a0cdb8c129d3efaabe4
Marco Siebecke <marco@siebecke.se>
2021-03-03 16:59:34 +0100

    initial import

---
+++ long_audio.py
@@ -0,0 +1,84 @@
+import logging
+import sys
+
+import click
+
+from google.cloud import speech_v1
+import io
+
+log = logging.getLogger(__file__)
+
+
+def sample_long_running_recognize(file_path: str,
+                                  model: str = 'video',
+                                  sample_rate: int = 16000,
+                                  language_code: str = 'en-US'):
+    """
+    Transcribe a long audio file using asynchronous speech recognition
+
+    Args:
+      file_path Path to audio file, e.g. /path/audio.wav or gcs://bucket/filename
+    """
+
+    client = speech_v1.SpeechClient()
+    config = {"language_code": language_code, "sample_rate_hertz": sample_rate, "model": model}
+
+    if not file_path.startswith('gs://'):
+        with io.open(file_path, "rb") as f:
+            content = f.read()
+            audio = {"content": content}
+    else:
+        audio = {"uri": file_path}
+
+    operation = client.long_running_recognize(config, audio)
+
+    print(u"Waiting for operation to complete...")
+    response = operation.result()
+
+    results = []
+    for result in response.results:
+        # First alternative is the most probable result
+        alternative = result.alternatives[0]
+        print(u"Transcript: {}".format(alternative.transcript))
+        results.append(alternative.transcript)
+
+    return results
+
+
+def _configure_logging(verbosity):
+    loglevel = max(3 - verbosity, 0) * 10
+    logging.basicConfig(level=loglevel, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    if loglevel >= logging.DEBUG:
+        # Disable debugging logging for external libraries
+        for loggername in 'urllib3', 'google.auth.transport.requests':
+            logging.getLogger(loggername).setLevel(logging.CRITICAL)
+
+
+@click.command()
+@click.option('-v', '--verbosity', help='Verbosity', default=0, count=True)
+@click.option('-l', '--language-code', help='langauge code', default="en-US")
+@click.option('-m', '-ml-model', help='ml model to use', default="video")
+@click.option('-s', '--sample-rate', help='sample rate', default=16000)
+@click.argument('filename', type=str)
+@click.argument('transcript-path', type=str)
+def cli(verbosity: int, language_code: str, ml_model: str, sample_rate: int, filename: str, transcript_path: str):
+    """ main program
+    """
+    _configure_logging(verbosity)
+
+    log.info("Transcribing %s", filename)
+    log.info("Writing transcript to %s", transcript_path)
+    transcript = sample_long_running_recognize(filename,
+                                               model=ml_model,
+                                               sample_rate=sample_rate,
+                                               language_code=language_code)
+    log.debug(transcript)
+    with open(transcript_path, 'w') as f:
+        f.write('\n'.join(transcript))
+
+    return 0
+
+
+if __name__ == '__main__':
+    # pylint: disable=E1120
+    sys.exit(cli())
